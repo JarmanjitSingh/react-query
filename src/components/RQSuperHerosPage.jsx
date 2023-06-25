531dff4337644a515afed90e87580903f0373bdf
JarmanjitSingh <jarmanjits176@gmail.com>
2023-06-25 23:41:23 +0530

    Custom Query Hook

--- src/components/RQSuperHerosPage.jsx
+++ src/components/RQSuperHerosPage.jsx
@@ -1,37 +1,22 @@
-import axios from 'axios'
-import React from 'react'
-import { useQuery } from 'react-query'
-
-const fetchSuperHeros = ()=>{
-  return axios.get("http://localhost:4000/superheroes")
-}
+import React from "react";
+import { useSuperHerosData } from "../hooks/useSuperHerosData";
 
 const RQSuperHerosPage = () => {
+  const successCallback = (data) => {
+    console.log("Data has been fetched successfully", data);
+  };
+  const errorCallback = (error) => {
+    console.log("There are some error in fetching", error);
+  };
+  const { isLoading, data, isError, error, isFetching } = useSuperHerosData(
+    successCallback,
+    errorCallback
+  );
 
-  const successCallback = (data)=>{
-    console.log("Data has been fetched successfully", data)
-  }
-  const errorCallback = (error)=>{
-    console.log("There are some error in fetching", error)
-  }
- const {isLoading, data, isError, error, isFetching} = useQuery("super-heros", fetchSuperHeros, {
-  cacheTime: 6000,
-  //staleTime:600000, //10mins    
-  refetchOnMount: false,
-  refetchOnWindowFocus: false,
-  onSuccess: successCallback,
-  onError: errorCallback,
-  select: (data) => {
-    const superHeroNames = data.data.map((hero)=> hero.name);
-    return superHeroNames
-  }
- })
+  console.log({ isLoading, isFetching });
 
-
- console.log({isLoading, isFetching});
-
-  if(isLoading) return <h2>Loading...</h2>
-  if(isError) return <h2>{error.message}</h2>
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>{error.message}</h2>;
   return (
     <div>
       <h2>RQ superheros page</h2>
@@ -41,13 +26,12 @@ const RQSuperHerosPage = () => {
         // })
 
         // # data Transformation
-        data.map((hero)=>{
-          return <div key={hero}>{hero}</div>
+        data.map((hero) => {
+          return <div key={hero}>{hero}</div>;
         })
-        
       }
     </div>
-  )
-}
+  );
+};
 
-export default RQSuperHerosPage
+export default RQSuperHerosPage;
--- src/components/SecondSameQuery.jsx
+++ src/components/SecondSameQuery.jsx
@@ -1,34 +1,42 @@
-import axios from 'axios'
-import React from 'react'
-import { useQuery } from 'react-query'
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
 
-const fetchSuperHeros = ()=>{
-  return axios.get("http://localhost:4000/superheroes")
-}
+const fetchSuperHeros = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 
 const SecondSameQuery = () => {
- const {isLoading, data, isError, error, isFetching} = useQuery("super-heros", fetchSuperHeros, {
-  cacheTime: 6000,
-  //staleTime:600000, //10mins    
-  refetchOnMount: false,
-  refetchOnWindowFocus: false
- })
+  const { isLoading, data, isError, error, isFetching } = useQuery(
+    "super-heros",
+    fetchSuperHeros,
+    {
+      cacheTime: 6000,
+      //staleTime:600000, //10mins
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    }
+  );
 
+  console.log({ isLoading, isFetching });
 
- console.log({isLoading, isFetching});
-
-  if(isLoading) return <h2>Loading...</h2>
-  if(isError) return <h2>{error.message}</h2>
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>{error.message}</h2>;
   return (
     <div>
-      <h2>Ok! so what we find if we fetching same data on multiple pages then make a unique key same on both pages then it will not refetcing the data for second page it will only mount the cached data</h2>
-      {
-        data?.data.map((hero)=>{
-          return <div key={hero.name}>{hero.name}</div>
-        })
-      }
+      <h2>
+        Ok! so what we find if we fetching same data on multiple pages then make
+        a unique key same on both pages then it will not refetcing the data for
+        second page it will only mount the cached data.
+        <br />
+        BUT! BUT! BUT! it is not the preferred way. the preffered way is custom
+        query hook
+      </h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default SecondSameQuery
+export default SecondSameQuery;
